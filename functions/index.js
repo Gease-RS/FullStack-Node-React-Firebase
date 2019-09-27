aa5858daaa31de692ef19ebbde8329608c4781c0
Gease-RS <geasebrasil@gmail.com>
2019-09-27 15:33:21 -0300

    Validacao

--- functions/index.js
+++ functions/index.js
@@ -47,15 +47,43 @@ app.get('/screams', (req, res) => {
         .catch((err) => console.error(err));
 })
 
-app.post('/screams', (req, res) => {
+const FBAuth = (req, res, next) => {
+    let idToken
+    if(req.headers.authorizantion && req.headers.authorizantion.startsWith('Bearer ')) {
+        idToken = req.headers.authorizantion.split('Bearer ')[1];
+    } else {
+        console.error('No token found')
+        return res.status(403).json({ error: 'Unauthorized'});
+    }
+
+    admin.auth().verifyIdToken(idToken)
+        .then(decodedToken => {
+            req.user = decodedToken;
+            return db.collection('users')
+                .where('userId', '==', req.user.uid)
+                .limit(1)
+                .get()
+    })
+    .then(data => {
+        req.user.handle = data.docs[0].data().handle;
+        //return res.status(200).json({ body: ''})
+        return next();
+    })
+    .catch(err => {
+        console.error('Error while verifying token ', err);
+        return res.status(403).json(err);
+    })
+}
+
+//Post one scream
+app.post('/screams', FBAuth, (req, res) => {
     const newScream = {
         body: req.body.body,
-        userHandle: req.body.userHandle,
+        userHandle: req.user.handle,
         createdAt: new Date().toISOString()
     };
 
-    db
-        .collection('screams')
+    db.collection('screams')
         .add(newScream)
         .then((doc) => {
             res.json({ message: `document ${doc.id} created successfully`});
