55c486d08c11f6eeb863a829d86c9552a6fcf6ea
Gease-RS <geasebrasil@gmail.com>
2019-09-27 10:25:49 -0300

    Funcao login

--- functions/index.js
+++ functions/index.js
@@ -11,13 +11,13 @@ admin.initializeApp({
 });
 
 const firebaseConfig = {
-    apiKey: "xxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-    authDomain: "xxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-    databaseURL: "xxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-    projectId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-    storageBucket: "xxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-    messagingSenderId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-    appId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+    apiKey: "AIzaSyDNSPSFXexkHCJSKG3nMQ46AnfPCGP5XQE",
+    authDomain: "fullstack-node-react-firebase.firebaseapp.com",
+    databaseURL: "https://fullstack-node-react-firebase.firebaseio.com",
+    projectId: "fullstack-node-react-firebase",
+    storageBucket: "fullstack-node-react-firebase.appspot.com",
+    messagingSenderId: "986472472459",
+    appId: "1:986472472459:web:5434b4b89fc4bfc6"
   };
 
 const firebase = require('firebase');
@@ -66,6 +66,17 @@ app.post('/screams', (req, res) => {
         });
 });
 
+const isEmail = (email) => {
+    const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if(email.match(regEx)) return true;
+    else return false;
+}
+
+const isEmpty = (string) => {
+    if(string.trim() === '') return true;
+    else return false;
+}
+
 //Sinup route
 app.post('/signup', (req, res) => {
     const newUser = {
@@ -75,6 +86,20 @@ app.post('/signup', (req, res) => {
         handle: req.body.handle,
     };
 
+    let errors = {};
+
+    if(isEmpty(newUser.email)) {
+        errors.email = 'Email must not be empty'
+    } else if(!isEmail(newUser.email)){
+        errors.email = 'Must be a valid email address'
+    }
+
+    if(isEmpty(newUser.password)) errors.password = 'Must not be empty'
+    if(newUser.password !== newUser.confirmPassword) errors.confirmPassword = 'Password must match';
+    if(isEmpty(newUser.handle)) errors.handle = 'Must not be empty';
+
+    if(Object.keys(errors).length > 0) return res.status(400).json(errors);
+
     let token, userId;
     db.doc(`/users/${newUser.handle}`)
         .get()
@@ -83,8 +108,8 @@ app.post('/signup', (req, res) => {
                 return res.status(400).json({ handle: 'this handle is already taken'});
             } else {
                 return firebase
-        .auth()
-        .createUserWithEmailAndPassword(newUser.email, newUser.password);
+                .auth()
+                .createUserWithEmailAndPassword(newUser.email, newUser.password);
         }
     })
         .then((data) => {
@@ -115,6 +140,40 @@ app.post('/signup', (req, res) => {
     })
 
 });
-//45:30
+
+app.post('/login', (req, res) => {
+    const user = {
+        email: req.body.email,
+        password: req.body.password
+    };
+
+    let errors = {};
+
+    if(isEmpty(user.email)) errors.email = 'Must not be empty';
+    if(isEmpty(user.password)) errors.password= 'Must not be empty';
+
+    if(Object.keys(errors).length > 0) return res.status(400).json(errors);
+
+    firebase
+        .auth()
+        .signInWithEmailAndPassword(user.email, user.password)
+        .then((data) => {
+            return data.user.getIdToken();
+        })
+        .then((token) => {
+            return res.json({ token });
+        })
+        .catch((err) => {
+            console.error(err);
+            if(err.code === 'auth/wrong-password') {
+                return res
+                    .status(403)
+                    .json({ general: 'Wrong credentials, please try again'});
+            } else return res
+                .status(500)
+                .json({ error: err.code});
+        })
+
+})
 
 exports.api = functions.https.onRequest(app);
