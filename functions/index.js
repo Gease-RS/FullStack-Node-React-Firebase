eaa79a41f4a5d499d28d59e8bd6dc46de103221d
Gease-RS <geasebrasil@gmail.com>
2019-10-01 00:03:36 -0300

    Funcao get user Details

--- functions/index.js
+++ functions/index.js
@@ -6,16 +6,24 @@ const cors = require('cors');
 app.use(cors());
 
 const { getAllScreams, postOneScream } = require('./handlers/screams');
-const { signup, login, uploadImage, addUserDetails } = require('./handlers/users');
+const { 
+    signup, 
+    login, 
+    uploadImage, 
+    addUserDetails,
+    getAuthenticatedUser
+} = require('./handlers/users');
 
 // Screams routes
 app.get('/screams', getAllScreams);
 app.post('/scream', FBAuth, postOneScream);
+
 
 // Users route
 app.post('/signup', signup);
 app.post('/login', login);
 app.post('/user/image', FBAuth, uploadImage);
 app.post('/user', FBAuth, addUserDetails);
+app.get('/user' , FBAuth, getAuthenticatedUser);
 
 exports.api = functions.region('europe-west1').https.onRequest(app);
