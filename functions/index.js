bed8ebbd092f69483825a67b674172f0f0c90733
Gease-RS <geasebrasil@gmail.com>
2019-10-02 03:35:26 -0300

    Deploy e Notifications sem registro no firestore

--- functions/index.js
+++ functions/index.js
@@ -13,6 +13,7 @@ const {
     likeScream,
     unlikeScream,
     deleteScream
+    
  } = require('./handlers/screams');
 
 const { 
@@ -39,4 +40,78 @@ app.post('/user/image', FBAuth, uploadImage);
 app.post('/user', FBAuth, addUserDetails);
 app.get('/user' , FBAuth, getAuthenticatedUser);
 
-exports.api = functions.region('europe-west1').https.onRequest(app);
+exports.api = functions.region('us-central1').https.onRequest(app);
+
+exports.createNotificationOnLike = functions
+  .region('us-central1')
+  .firestore.document('likes/{id}')
+  .onCreate((snapshot) => {
+    return db
+      .doc(`/screams/${snapshot.data().screamId}`)
+      .get()
+      .then((doc) => {
+        if (doc.exists){
+          return db.doc(`/notifications/${snapshot.id}`).set({
+            createdAt: new Date().toISOString(),
+            recipient: doc.data().userHandle,
+            sender: snapshot.data().userHandle,
+            type: 'like',
+            read: false,
+            screamId: doc.id
+          });
+        }
+      })
+      .then(() => {
+          return;
+      })
+      .catch((err) => {
+          console.error(err);
+          return;
+       });
+  });
+
+exports.deleteNotificationOnUnLike = functions
+.region('us-central1')
+.firestore.document('likes/{id}')
+.onDelete((snapshot) => {
+    return db
+      .doc(`/notifications/${snapshot.id}`)
+      .delete()
+      .then(() => {
+          return;
+      })
+      .catch((err) => {
+        console.error(err);
+        return;
+      });
+  });
+  
+exports.createNotificationOnComment = functions
+  .region('us-central1')
+  .firestore.document('comments/{id}')
+  .onCreate((snapshot) => {
+    return db
+      .doc(`/screams/${snapshot.data().screamId}`)
+      .get()
+      .then((doc) => {
+        if (doc.exists){
+          return db.doc(`/notifications/${snapshot.id}`).set({
+            createdAt: new Date().toISOString(),
+            recipient: doc.data().userHandle,
+            sender: snapshot.data().userHandle,
+            type: 'comment',
+            read: false,
+            screamId: doc.id
+          });
+        }
+      })
+      .then(() => {
+          return;
+      })
+      .catch((err) => {
+          console.error(err);
+          return;
+       });
+  });
+
+  
